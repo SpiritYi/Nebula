933bf389c23a610333a0969298ff23592e6d3d3c
SpiritYi <jinglingyueyue@gmail.com>
2018-05-29 13:56:20 +0800

    db test

--- cron/dev/CodeTest.php
+++ cron/dev/CodeTest.php
@@ -7,11 +7,45 @@
  */
 
 require_once dirname(__FILE__) . '/../../config/DirConfig.inc.php';
+require_once CODE_BASE . '/app/stock/model/StockCompanyModel.class.php';
 
 class CodeTest {
     public function run() {
+        $this->dbTest();
         // $this->msg();
-        $this->info();
+//        $this->info();
+//        $this->esResquest();
+    }
+
+    public function dbTest() {
+        $startT = microtime(true);
+        for ($i = 0; $i < 10; $i ++) {
+            $count = StockCompanyModel::getCompanyCount();
+            var_dump($count);
+        }
+        $endT = microtime(true);
+        var_dump('time:' . ($endT - $startT) . 's');
+    }
+
+    public function esResquest() {
+//        require_once CODE_BASE . '/util/http/EsUtil.class.php';
+//        $res = EsUtil::curlRequest('http://es.nebula-fund.com/leonebula-test/backend/1/_update', 'POST', json_encode(['doc' => ['phone' => '123']]));
+//        var_dump($res);exit;
+
+        require_once CODE_BASE . '/app/es/LeoEsNamespace.class.php';
+        $data = array(
+            'id' => '1003',
+            'name' => '肌肉试剂',
+            'address' => '北京朝阳SOHO',
+            'phone' => '13287638298',
+            'keyword' => '肌肉 染色',
+        );
+//        $res = LeoEsNamespace::addRecord('1003', $data);
+
+//        $res = LeoEsNamespace::delRecord('*');
+
+        $res = LeoEsNamespace::updateRecord('1002', ['address' => '上海虹桥机场T2 102室']);
+        var_dump($res);
     }
 
     public function msg() {
